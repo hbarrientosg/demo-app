c457a96cd10f223faaa329b9f3819b5e792ee125
Humberto Barrientos <humberto@drieam.com>
2020-03-14 20:21:55 +0100

    add analyze token

--- scripts/analyze.js
+++ scripts/analyze.js
@@ -5,7 +5,7 @@ const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin')
 const webpackConfigProd = require("react-scripts/config/webpack.config")();
 
 webpackConfigProd.plugins.push(
-  new BundleAnalyzerPlugin({ token: 'a5b1129a025fd1d92a99f5' })
+  new BundleAnalyzerPlugin({ token: 'a5b1129a025fd1d92a99f570dbd9a52765b92474' })
 )
 
 require("react-scripts/scripts/build")
