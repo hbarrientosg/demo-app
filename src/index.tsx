7804fa4a0db96d92a0a6e0341dd3e2d84052d2db
Humberto Barrientos <humberto@drieam.com>
2020-03-15 10:37:35 +0100

    rename to DemoApp

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import DemoApp from './App';
+import DemoApp from './DemoApp';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<DemoApp />, document.getElementById('root'));
